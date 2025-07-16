dfb02e0363a4cf3177663cfd15a9ef600532144b
Samuel Leiton N <samleighton02@gmail.com>
2025-07-15 23:40:36 -0600

    v1.0.1

--- WPBackendDash.php
+++ WPBackendDash.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Backend Dash
 Description: Backend dashboard y control de entrevistas para AI.
-Version: 1.0
+Version: 1.0.1
 Author: SamLeiNav
 GitHub Plugin URI: https://github.com/Samleinav/WP-BackendDash
 */
